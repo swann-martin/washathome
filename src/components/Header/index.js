35150a77f290c403137cd071630c07bd54c80115
Sunburst009 <nicu.cesari@gmail.com>
2021-07-16 16:19:01 +0200

    changed div to header in header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './style.scss';
 
 const Header = () => (
-  <div className="header">
+  <header className="header">
     <h2>Header</h2>
-  </div>
+  </header>
 );
 export default Header;
