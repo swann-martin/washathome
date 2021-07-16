389dd60e2542bccd46c02332221a0ef6b9bdac44
swann-martin <swannmartin1988@gmail.com>
2021-07-16 16:00:48 +0200

    changed css to scss created Header component in App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,17 @@
 // == Import npm
 import React from 'react';
+import Header from '../Header';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
     <h1>Composant : App</h1>
+    <p>test</p>
+    <Header />
   </div>
 );
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.scss';
+
+const Header = () => (
+  <div className="header">
+    <h2>Header</h2>
+  </div>
+);
+export default Header;
