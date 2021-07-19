1603e24fdc5fe809655f3c6ddcae222cc9327f2a
swann-martin <swannmartin1988@gmail.com>
2021-07-19 17:29:24 +0200

    solved issue with margin between nav and header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,18 +2,18 @@
 import React, { useState } from 'react';
 import Header from '../Header';
 import Nav from '../Nav';
-import Connect from '../Connect'
+import Connect from '../Connect';
 // == Import
 
 import './styles.scss';
 
 // == Composant
 const App = () => {
-  const [isConnected, setIsConnected] = useState(false)
+  const [isConnected, setIsConnected] = useState(false);
 
   const handleIsConnected = () => {
-    setIsConnected(!isConnected)
-  }
+    setIsConnected(!isConnected);
+  };
   return (
     <div className="app">
       <Nav isConnected={isConnected} handleIsConnected={handleIsConnected} />
@@ -21,6 +21,6 @@ const App = () => {
       {isConnected && <Connect isConnected={isConnected} handleIsConnected={handleIsConnected} />}
     </div>
   );
-}
+};
 // == Export
 export default App;
--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -2,18 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
-const Connect = ({ handleIsConnected }) => {
-    return (
-        <div className="connect">
-            <div className="connect-btn" title="fermer cette fenêtre" onClick={() => { console.log('fermer le composant login'); handleIsConnected() }}><i className="fas fa-times"></i></div>
-            <form className="connect-form" onSubmit={(evt) => { evt.preventDefault() }}>
-                <input className="connect-form-input" type="text" name="login" placeholder="login" />
-                <input type="password" className="connect-form-input" name="password" placeholder="mot de passe" />
-                <button type="submit" className="connect-form-btn" onClick={() => { console.log('clicked on se connecter'); handleIsConnected() }}>Se connecter</button>
-                <p className="connect-form-text">Ou</p>
-                <Link className="connect-form-register" to='/register' onClick={() => { handleIsConnected() }}>S'inscrire</Link >
-            </form>
-        </div>
-    )
-}
-export default Connect
+const Connect = ({ handleIsConnected }) => (
+  <div className="connect">
+    <div
+      className="connect-btn"
+      title="fermer cette fenêtre"
+      onClick={() => {
+        console.log('fermer le composant login'); handleIsConnected();
+      }}
+    ><i className="fas fa-times" />
+    </div>
+    <form
+      className="connect-form"
+      onSubmit={(evt) => {
+        evt.preventDefault();
+      }}
+    >
+      <input className="connect-form-input" type="text" name="login" placeholder="login" />
+      <input type="password" className="connect-form-input" name="password" placeholder="mot de passe" />
+      <button
+        type="submit"
+        className="connect-form-btn"
+        onClick={() => {
+          console.log('clicked on se connecter'); handleIsConnected();
+        }}
+      >Se connecter
+      </button>
+      <p className="connect-form-text">Ou</p>
+      <Link
+        className="connect-form-register"
+        to="/register"
+        onClick={() => {
+          handleIsConnected();
+        }}
+      >S'inscrire
+      </Link>
+    </form>
+  </div>
+);
+export default Connect;
