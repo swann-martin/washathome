04bd24980d1a491cc6ef342f4a50d184e4a4c0cd
swann-martin <swannmartin1988@gmail.com>
2021-07-19 17:11:31 +0200

    added props to change state of component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header';
 import Nav from '../Nav';
 import Connect from '../Connect'
@@ -9,11 +9,16 @@ import './styles.scss';
 
 // == Composant
 const App = () => {
+  const [isConnected, setIsConnected] = useState(false)
+
+  const handleIsConnected = () => {
+    setIsConnected(!isConnected)
+  }
   return (
     <div className="app">
-      <Nav />
+      <Nav isConnected={isConnected} handleIsConnected={handleIsConnected} />
       <Header />
-      <Connect />
+      {isConnected && <Connect isConnected={isConnected} handleIsConnected={handleIsConnected} />}
     </div>
   );
 }
--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
-const Connect = () => (
-    <div className="connect">
-        <div className="connect-btn" title="fermer cette fenêtre" onClick={() => { console.log('fermer le composant login') }}><i className="fas fa-times"></i></div>
-        <form className="connect-form" onSubmit={(evt) => { evt.preventDefault() }}>
-            <input className="connect-form-input" type="text" name="login" placeholder="login" />
-            <input type="password" className="connect-form-input" name="password" placeholder="mot de passe" />
-            <button type="submit" className="connect-form-btn" onClick={() => { console.log('clicked on se connecter') }}>Se connecter</button>
-            <p className="connect-form-text">Ou</p>
-            <Link className="connect-form-register" to='/register'>S'inscrire</Link >
-        </form>
-    </div>
-
-)
+const Connect = ({ handleIsConnected }) => {
+    return (
+        <div className="connect">
+            <div className="connect-btn" title="fermer cette fenêtre" onClick={() => { console.log('fermer le composant login'); handleIsConnected() }}><i className="fas fa-times"></i></div>
+            <form className="connect-form" onSubmit={(evt) => { evt.preventDefault() }}>
+                <input className="connect-form-input" type="text" name="login" placeholder="login" />
+                <input type="password" className="connect-form-input" name="password" placeholder="mot de passe" />
+                <button type="submit" className="connect-form-btn" onClick={() => { console.log('clicked on se connecter'); handleIsConnected() }}>Se connecter</button>
+                <p className="connect-form-text">Ou</p>
+                <Link className="connect-form-register" to='/register' onClick={() => { handleIsConnected() }}>S'inscrire</Link >
+            </form>
+        </div>
+    )
+}
 export default Connect
