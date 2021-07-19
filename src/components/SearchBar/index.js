45b888ad10d94d7213dd56ed01ed9a3da292787a
Sunburst009 <nicu.cesari@gmail.com>
2021-07-19 14:14:28 +0200

    search logo button added scss completed

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -3,9 +3,20 @@ import React from 'react';
 import './styles.scss';
 
 const SearchBar = () => (
-  <form className="searchbar">
-    <input className="searchbar-bar" type="search" placeholder="Entre ton code postal..." />
-    <input className="search-button" type="submit" value="Rechercher" />
+  <form
+    className="searchbar"
+  >
+    <input
+      className="searchbar-bar"
+      type="search"
+      placeholder="Entre ton code postal..."
+    />
+    <button
+      type="submit"
+      className="searchbar-button"
+    >
+      <i className="fas fa-search-location" />
+    </button>
   </form>
 );
 
