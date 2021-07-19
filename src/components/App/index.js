fef71f9170a5365bd49d19d07f3f53d97c1f1031
swann-martin <swannmartin1988@gmail.com>
2021-07-19 17:00:29 +0200

    added component connect with style

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,17 +2,20 @@
 import React from 'react';
 import Header from '../Header';
 import Nav from '../Nav';
+import Connect from '../Connect'
 // == Import
 
 import './styles.scss';
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <Nav />
-    <Header />
-  </div>
-);
-
+const App = () => {
+  return (
+    <div className="app">
+      <Nav />
+      <Header />
+      <Connect />
+    </div>
+  );
+}
 // == Export
 export default App;
