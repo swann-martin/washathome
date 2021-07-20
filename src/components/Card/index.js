4baa4e0b05c8a1612a0d3238514bcd340f21dedf
Sunburst009 <nicu.cesari@gmail.com>
2021-07-20 15:29:44 +0200

    main structure

---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Card = () => (
+  <article>
+    <img/>
+    <div>
+      <p>Ville</p>
+      <p>Distance</p>
+      <p>Prix</p>
+      <p>Note/5</p>
+    </div>
+  </article>
+);
+
+export default Card;
