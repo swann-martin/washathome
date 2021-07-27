70eee8b7eec5a79e1efa5771ddba0cf9ddc90e3d
swann-martin <swannmartin1988@gmail.com>
2021-07-27 13:32:32 +0200

    deleted hooks for opening and closing menu in app and menu, now we use redux

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,9 +2,11 @@ export const INPUT_CHANGE = 'INPUT_CHANGE';
 export const LOGIN_FORM_SUBMIT = 'LOGIN_FORM_SUBMIT';
 export const USER_LOGOUT = 'USER_LOGOUT';
 
+
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const TOGGLE_CONNECTED = 'TOGGLE_CONNECTED';
+export const TOGGLE_MENU = 'TOGGLE_MENU';
 export const REGISTER_USER_INPUT_CHANGE = 'REGISTER_USER_INPUT_CHANGE';
 export const REGISTER_USER_FORM_SUBMIT = 'REGISTER_USER_FORM_SUBMIT';
 
@@ -42,6 +44,10 @@ export const loginError = () => ({
 
 export const toggleConnected = () => ({
   type: TOGGLE_CONNECTED,
+});
+
+export const toggleMenu = () => ({
+  type: TOGGLE_MENU,
 });
 
 export const registerUserInputChange = (payload) => ({
